ec0f886fa592c6a741a48298eefb31948af55c7d
testwebportalmercedes <testwebportalmercedes@gmail.com>
2023-01-29 19:01:35 +0300

    Перенесены некоторые методы из base_page в main_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,14 @@
 from selenium.common.exceptions import NoSuchElementException
 from .locators import ProductPageLocators
 from .locators import BasePageLocator
+from .locators import BasketPageLocator
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+
 class BasePage:
-    def __init__(self, browser, url, timeout = 10):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
@@ -13,6 +16,20 @@ class BasePage:
     def open(self):
         self.browser.get(self.url)
 
+    # //////////////////////////////////////// Переход на главную страницу с любой страницы
+    def go_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocator.LOGIN_LINK)
+        login_link.click()
+
+    # ///////////////////////////////////////////////////  Переход в корзину с любой страницы
+    def go_to_backet(self):
+        go_to_backet = self.browser.find_element(*BasketPageLocator.GO_TO_BACKET)
+        go_to_backet.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocator.LOGIN_LINK)
+
+    # //////////////////////////////////////////////// Проверка что элемент присутствует
     def is_element_present(self, how, what):
 
         try:
@@ -20,13 +37,6 @@ class BasePage:
         except NoSuchElementException:
             return False
         return True
-    # //////////////////////////////////////// Перенесено из main_page так как много где применяется
-    def go_login_page(self):
-        login_link = self.browser.find_element(*BasePageLocator.LOGIN_LINK)
-        login_link.click()
-
-    def should_be_login_link(self):
-        assert self.is_element_present(*BasePageLocator.LOGIN_LINK)
 
     # //////////////////////////////////////////////// Проверка что элемент отсутствует
     def is_not_element_present(self, how, what, timeout=4):
@@ -37,16 +47,14 @@ class BasePage:
 
         return False
 
-    def should_not_be_success_message(self):
-        assert self.is_not_element_present(*ProductPageLocators.NAME_AFTER_ADD), \
-            "Success message is presented, but should not be"
-
+    # //////////////////////////////////////////////// Проверка что элемент исчезает
     def is_disappeared(self, how, what, timeout=4):
         try:
-            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
                 until_not(EC.presence_of_element_located((how, what)))
         except TimeoutException:
             return False
 
         return True
+
 
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,7 @@
 from .base_page import BasePage
 
 from selenium.webdriver.common.by import By
-from .locators import MainPageLocators
+from .locators import BasePageLocator
 
 class MainPage(BasePage):
 
